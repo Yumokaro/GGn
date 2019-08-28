bcd5d28960ae18383a82d1c95dec2461f8c59411
Yumokaro <43580935+Yumokaro@users.noreply.github.com>
2019-08-28 09:44:39 -0700

    initial directory upload

---
+++ consoleList.py
@@ -0,0 +1,105 @@
+# Console list array
+
+
+mainConsoleList = [
+    # Return Microsoft
+    ['microsoftXBOX', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Xbox&order_by=groupname&order_way=asc'],
+    ['microsoftXbox360', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Xbox+360&order_by=groupname&order_way=asc'],
+    ['microsoftWindows', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Windows&order_by=groupname&order_way=asc'],
+    ['microsoftMSX', 'https://gazellegames.net/torrents.php?searchstr=&artistname=MSX&order_by=groupname&order_way=asc'],
+    ['microsoftMSX2', 'https://gazellegames.net/torrents.php?searchstr=&artistname=MSX+2&order_by=groupname&order_way=asc'],
+    # Return Nintendo
+    ['nintendoGameBoy', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Game+Boy&order_by=groupname&order_way=asc'],
+    ['nintendoGameBoyAdvance', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Game+Boy+Advance&order_by=groupname&order_way=asc'],
+    ['nintendoGameBoyColor', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Game+Boy+Color&order_by=groupname&order_way=asc'],
+    ['nintendoEntertainmentSystem', 'https://gazellegames.net/torrents.php?searchstr=&artistname=NES&order_by=groupname&order_way=asc'],
+    ['nintendo64', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Nintendo+64&order_by=groupname&order_way=asc'],
+    ['nintendo3DS', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Nintendo+3DS&order_by=groupname&order_way=asc'],
+    ['nintendo3DSNew', 'https://gazellegames.net/torrents.php?searchstr=&artistname=New+Nintendo+3DS&order_by=groupname&order_way=asc'],
+    ['nintendoDS', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Nintendo+DS&order_by=groupname&order_way=asc'],
+    ['nintendoGameCube', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Nintendo+GameCube&order_by=groupname&order_way=asc'],
+    ['nintendoPokemonMini', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Pokemon+Mini&order_by=groupname&order_way=asc'],
+    ['superNintendo', 'https://gazellegames.net/torrents.php?searchstr=&artistname=SNES&order_by=groupname&order_way=asc'],
+    ['nintendoSwitch', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Switch&order_by=groupname&order_way=asc'],
+    ['nintendoVirtualBoy', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Virtual+Boy&order_by=groupname&order_way=asc'],
+    ['nintendoWII', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Wii&order_by=groupname&order_way=asc'],
+    ['nintendoWIIU', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Wii+U&order_by=groupname&order_way=asc'],
+    # Return Sony
+    ['playstation1', 'https://gazellegames.net/torrents.php?searchstr=&artistname=PlayStation+1&order_by=groupname&order_way=asc'],
+    ['playstation2', 'https://gazellegames.net/torrents.php?searchstr=&artistname=PlayStation+2&order_by=groupname&order_way=asc'],
+    ['playstation3', 'https://gazellegames.net/torrents.php?searchstr=&artistname=PlayStation+3&order_by=groupname&order_way=asc'],
+    ['playstation4', 'https://gazellegames.net/torrents.php?searchstr=&artistname=PlayStation+4&order_by=groupname&order_way=asc'],
+    ['playstationPortable', 'https://gazellegames.net/torrents.php?searchstr=&artistname=PlayStation+Portable&order_by=groupname&order_way=asc'],
+    ['playstationVita', 'https://gazellegames.net/torrents.php?searchstr=&artistname=PlayStation+Vita&order_by=groupname&order_way=asc'],
+    # Return Sega
+    ['segaDreamCast', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Dreamcast&order_by=groupname&order_way=asc'],
+    ['segaGameGear', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Game+Gear&order_by=groupname&order_way=asc'],
+    ['segaMasterSystem', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Master+System&order_by=groupname&order_way=asc'],
+    ['segaMegaDrive', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Mega+Drive&order_by=groupname&order_way=asc'],
+    ['segaPico', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Pico&order_by=groupname&order_way=asc'],
+    ['segaSaturn', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Saturn&order_by=groupname&order_way=asc'],
+    ['segaSG1000', 'https://gazellegames.net/torrents.php?searchstr=&artistname=SG-1000&order_by=groupname&order_way=asc'],
+    # Atari
+    ['atari 2600', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Atari+2600&order_by=groupname&order_way=asc'],
+    ['atari 5200', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Atari+5200&order_by=groupname&order_way=asc'],
+    ['atari 7800', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Atari+7800&order_by=groupname&order_way=asc'],
+    ['atari Jaguar', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Atari+Jaguar&order_by=groupname&order_way=asc'],
+    ['atari Lynx', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Atari+Lynx&order_by=groupname&order_way=asc'],
+    ['atari ST', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Atari+ST&order_by=groupname&order_way=asc'],
+]
+
+secondaryConsoleList = [
+    # Return Sinclair
+    ['sinclairZXSpectrum', 'https://gazellegames.net/torrents.php?searchstr=&artistname=ZX+Spectrum&order_by=groupname&order_way=asc'],
+    # Return Tiger
+    ['tigerGameCom', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Game.com&order_by=groupname&order_way=asc'],
+    # Return VTech
+    ['vTechVSmile', 'https://gazellegames.net/torrents.php?searchstr=&artistname=V.Smile&order_by=groupname&order_way=asc'],
+    ['vTechCreaiVision', 'https://gazellegames.net/torrents.php?searchstr=&artistname=CreatiVision&order_by=groupname&order_way=asc'],
+    # Return Bandai
+    ['bandaiWonderSwan', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Bandai+WonderSwan&order_by=groupname&order_way=asc'],
+    ['bandaiWonderSwanColor', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Bandai+WonderSwan+Color&order_by=groupname&order_way=asc'],
+    # Return Casio
+    ['casioPV1000', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Casio+PV-1000&order_by=groupname&order_way=asc'],
+    # Return Coleco
+    ['colecoColecoVision', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Colecovision&order_by=groupname&order_way=asc'],
+    # Return Commodore
+    ['commodore64', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Commodore+64&order_by=groupname&order_way=asc'],
+    ['commodoreAmiga', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Commodore+Amiga&order_by=groupname&order_way=asc'],
+    ['commodorePlus4', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Commodore+Plus-4&order_by=groupname&order_way=asc'],
+    ['commodoreVIC20', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Commodore+VIC-20&order_by=groupname&order_way=asc'],
+    # Return Emerson
+    ['emersonArcadia2001', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Emerson+Arcadia+2001&order_by=groupname&order_way=asc'],
+    # Return Entex
+    ['entexAdventureVision', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Entex+Adventure+Vision&order_by=groupname&order_way=asc'],
+    # Return Epoch
+    ['epochSuperCasetteVision', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Epoch+Super+Casette+Vision&order_by=groupname&order_way=asc'],
+    # Return Fairchild
+    ['fairchildChannelF', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Fairchild+Channel+F&order_by=groupname&order_way=asc'],
+    # Return Funtech
+    ['funtechSuperAcan', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Funtech+Super+Acan&order_by=groupname&order_way=asc'],
+    # Return Gamepark
+    ['gameparkGP32', 'https://gazellegames.net/torrents.php?searchstr=&artistname=GamePark+GP32&order_by=groupname&order_way=asc'],
+    # Return General Computer Entertainment
+    ['gceVectrex', 'https://gazellegames.net/torrents.php?searchstr=&artistname=General+Computer+Vectrex&order_by=groupname&order_way=asc'],
+    # Return Hartung
+    ['hartungGameMaster', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Hartung+Game+Master&order_by=groupname&order_way=asc'],
+    # Return Magnavox
+    ['magnavoxOdyssey', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Magnavox-Phillips+Odyssey&order_by=groupname&order_way=asc'],
+    # Return NEC
+    ['necSuperGrafx', 'https://gazellegames.net/torrents.php?searchstr=&artistname=NEC+SuperGrafx&order_by=groupname&order_way=asc'],
+    ['necTurboGrafx16', 'https://gazellegames.net/torrents.php?searchstr=&artistname=NEC+TurboGrafx-16&order_by=groupname&order_way=asc'],
+    # Return Nokia
+    ['nokiaNGage', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Nokia+N-Gage&order_by=groupname&order_way=asc'],
+    # Return Philips
+    ['philipsVideopac', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Philips+Videopac%2B&order_by=groupname&order_way=asc'],
+    # Return RCA
+    ['rcaStudioII', 'https://gazellegames.net/torrents.php?searchstr=&artistname=RCA+Studio+II&order_by=groupname&order_way=asc'],
+    # Return SNK
+    ['snkNeoGeo', 'https://gazellegames.net/torrents.php?searchstr=&artistname=SNK+Neo+Geo&order_by=groupname&order_way=asc'],
+    ['snkNeoGeoPocket', 'https://gazellegames.net/torrents.php?searchstr=&artistname=SNK+Neo+Geo+Pocket&order_by=groupname&order_way=asc'],
+    # Return Watara
+    ['wataraSupervision', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Watara+Supervision&order_by=groupname&order_way=asc']
+]
+
+print('consoleList module loaded')
---
+++ dlTorrentsByConsole.py
@@ -0,0 +1,88 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+import GGn.consoleList
+import time
+
+
+
+# Open web browser window
+browser = webdriver.Chrome('D:\Downloads\chromedriver')
+browser.get('https://gazellegames.net/login.php')
+time.sleep(4)
+
+# Login Form
+browser.find_element_by_id('username').send_keys('Enter your Username here')
+
+browser.find_element_by_id('password').send_keys('Enter your password here')
+
+browser.find_element_by_id('keeplogged').click()
+
+time.sleep(5)
+
+browser.find_element_by_id('password').send_keys(Keys.ENTER)
+
+time.sleep(30)
+
+##################################################
+##################################################
+##################################################
+
+# Visit Profile
+# browser.get('https://gazellegames.net/user.php?id=')
+time.sleep(10)
+
+
+# Use consoleList array of second file, consoleList.py
+# list = consoleList.secondaryConsoleList
+
+# Example - Use a single console
+list = [ ['atari 2600', 'https://gazellegames.net/torrents.php?searchstr=&artistname=Atari+2600&order_by=groupname&order_way=asc'] ]
+print('Acquired console list')
+time.sleep(5)
+
+consoleComplete = []
+# Begin scrape
+consoleNumber = 0
+for x in list:
+    # Go to this console in the list and pull the second entry of the first item of the array - link
+    browser.get(list[consoleNumber][1])
+    print('Downloading torrents for: ', list[consoleNumber][0])
+    time.sleep(15)
+
+    t = 0
+    e = 0
+    while (e < 1):
+        try:
+            browser.find_elements_by_link_text('DL')[t].click()
+            print('Download number:', (t + 1))
+            time.sleep(4)
+            t += 1
+
+        except IndexError:
+            print('No more torrents on this page.')
+            print('This page is finished, moving to next page')
+
+            #next = 0
+            #while next < 1:
+            try:
+                browser.find_element_by_link_text('Next >').click()
+                time.sleep(5)
+                t = 0
+            except:
+                print('This console is finished')
+                time.sleep(5)
+                #next = 1
+                t = 0
+                e = 1
+
+    print('<<< Moving on to the next console >>>')
+    consoleNumber += 1
+
+
+# Shut down
+browser.close()
+browser.quit()
+
+print("Finished")
